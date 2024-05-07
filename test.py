27d9d7b3ebe63978f40e903c1cf587d1f2d7d847
PAAPIMERD <97976194+PAAPIMERD@users.noreply.github.com>
2024-05-07 21:35:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,32 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+
+def send_email(sender_email, sender_password, receiver_email, subject, body):
+    # Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
+
+    # Add message body
+    message.attach(MIMEText(body, 'plain'))
+
+    # Connect to the server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    # Login to your gmail account
+    server.login(sender_email, sender_password)
+
+    # Send Email
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+    # Quit the server
+    server.quit()
+
 while True:
-  time.sleep(60)
+  time.sleep(1)
   sender_email = "rishavi080720002@gmail.com"
   sender_password = "vbgi mtxh gnfw fzoq"
   receiver_email = "avinash9588@gmail.com"
